b51223f19e09d5c47ef9ba6cfdeb7036a7e65183
IonutPad15 <ionutpad15@gmail.com>
2021-10-31 13:41:52 +0200

    Am adaugat gui-ul pentru meniul principal, Health&Happiness si Meserii. Totodata, mici modificari la ScriereFisier

---
+++ ProiectMititeanPadureanSimon/src/Ionut/GUI/MeniuPrincipal.java
@@ -0,0 +1,147 @@
+package Ionut.GUI;
+
+import Ionut.Threads.*;
+import Ionut.classes.CitireFisier;
+import Ionut.classes.LifeSimulator;
+import Cristina.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class MeniuPrincipal extends CitireFisier {
+    private JPanel panelMeniuP;
+    private JPanel panelStatus;
+    private JLabel labelNume;
+    private JLabel iconHh;
+    private JLabel labelHh;
+    private JLabel iconBani;
+    private JLabel labelBani;
+    private JLabel labelData;
+    private JPanel panelImg;
+    private JPanel panelButoane;
+    private JButton butonMeserii;
+    private JButton butonCasa;
+    private JButton butonHh;
+    private JButton butonStudii;
+    private JLabel Imagine;
+    private JFrame jFrame;
+    private LifeSimulator ls = LifeSimulator.getInstance();
+    private static MeniuPrincipal _instance;
+    private MeniuPrincipal(){
+        /*if(SwingUtilities.isEventDispatchThread())
+        {
+            prepareFrame();
+        }
+        else
+        {
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    prepareFrame();
+                }
+            });
+        }*/
+        prepareFrame();
+    }
+    public static MeniuPrincipal getInstance()
+    {
+        if(_instance == null) _instance = new MeniuPrincipal();
+        return _instance;
+    }
+    private void prepareFrame()
+    {
+        jFrame = new JFrame("LifeSimulator");
+        jFrame.setContentPane(panelMeniuP);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.pack();
+        jFrame.setSize(550, 300);
+        jFrame.setLocation(650, 250);
+        jFrame.setVisible(false);
+        jFrame.setResizable(false);
+        jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        //DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        //LocalDateTime now = LocalDateTime.now();
+        //labelData.setText(dtf.format(now)+"");
+        String s = citireFisierString("ProiectMititeanPadureanSimon/src/Ionut/resources/Data", "succes", "eroare");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate  ld = LocalDate.parse(s, formatter);
+        labelData.setText(formatter.format(ld)+"");
+        int bani = citireFisierInt("ProiectMititeanPadureanSimon/src/Ionut/resources/Bani.txt","succes","eroare");
+        labelBani.setText(bani + "");
+        s = citireFisierString("ProiectMititeanPadureanSimon/src/Ionut/resources/Nume","succes","eroare");
+        labelNume.setText(s);
+        int procenthh = citireFisierInt("ProiectMititeanPadureanSimon/src/Ionut/resources/HH.txt","succes","eroare");
+        labelHh.setText(""+procenthh);
+        ImageIcon img = new ImageIcon(new ImageIcon("ProiectMititeanPadureanSimon/src/Ionut/resources/Hh.png").getImage().getScaledInstance(75,55,Image.SCALE_DEFAULT));
+        iconHh.setIcon(img);
+        img = new ImageIcon(new ImageIcon("ProiectMititeanPadureanSimon/src/Ionut/resources/moneyIcon.png").getImage().getScaledInstance(75,65,Image.SCALE_DEFAULT));
+        iconBani.setIcon(img);
+        img = new ImageIcon(new ImageIcon("ProiectMititeanPadureanSimon/src/Ionut/resources/workIcon.png").getImage().getScaledInstance(35,35,Image.SCALE_DEFAULT));
+        butonMeserii.setIcon(img);
+        img = new ImageIcon(new ImageIcon("ProiectMititeanPadureanSimon/src/Ionut/resources/homeIcon.png").getImage().getScaledInstance(35,35,Image.SCALE_DEFAULT));
+        butonCasa.setIcon(img);
+        img = new ImageIcon(new ImageIcon("ProiectMititeanPadureanSimon/src/Ionut/resources/EducationIcon.png").getImage().getScaledInstance(45,35,Image.SCALE_DEFAULT));
+        butonStudii.setIcon(img);
+        img = new ImageIcon(new ImageIcon("ProiectMititeanPadureanSimon/src/Ionut/resources/hhIcon.png").getImage().getScaledInstance(45,35,Image.SCALE_DEFAULT));
+        butonHh.setIcon(img);
+        butonMeserii.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                jFrame.setVisible(false);
+                Meseriigui m = Meseriigui.getInstance();
+                //SwingUtilities.invokeLater(m);
+                m.getjFrame().setVisible(true);
+            }
+        });
+        butonHh.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                jFrame.setVisible(false);
+                Hh h = Hh.getInstance();
+                //SwingUtilities.invokeLater(m);
+                h.getjFrame().setVisible(true);
+            }
+        });
+        /*butonCasa.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                jFrame.setVisible(false);
+                CasaGUI c = CasaGUI.getInstance();
+                //SwingUtilities.invokeLater(m);
+                c.getjFrame().setVisible(true);
+            }
+        });*/
+    }
+    public JFrame getjFrame()
+    {
+        return jFrame;
+    }
+
+    public JLabel getLabelData() {
+        return labelData;
+    }
+
+    public static void main(String[] args) {
+        MeniuPrincipal mp = MeniuPrincipal.getInstance();
+        mp.getjFrame().setVisible(true);
+        Thread1 r1 = new Thread1();
+        Thread t1 = new Thread(r1);
+        ThreadMinus r2 = new ThreadMinus();
+        Thread t2 = new Thread(r2);
+        t1.start();
+        t2.start();
+    }
+    public JLabel getLabelBani()
+    {
+        return labelBani;
+    }
+    public JLabel getLabelNume(){ return labelNume;}
+    public JLabel getLabelHh()
+    {
+        return labelHh;
+    }
+}
---
+++ ProiectMititeanPadureanSimon/src/Ionut/Threads/ThreadMinus.java
@@ -0,0 +1,122 @@
+package Ionut.Threads;
+
+import Ionut.GUI.Hh;
+import Ionut.GUI.MeniuPrincipal;
+import Ionut.GUI.Meseriigui;
+import Ionut.classes.CitireFisier;
+import Ionut.classes.LifeSimulator;
+import Cristina.*;
+
+import javax.swing.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+////scade datele si le scrie
+public class ThreadMinus extends CitireFisier implements Runnable{
+    private MeniuPrincipal mp;
+    private Hh hh;
+    private Meseriigui meseriigui;
+    private JLabel datalabelpp;
+    private JLabel banilabelpp,  banilabelMeserii, banilabelhh, banilabelCasa;
+    private JLabel hhlabelMeserii, hhlabelMp, hhlabelCasa, hhlabelHh;
+    //private CasaGUI casagui;
+    private String s ;
+    private DateTimeFormatter formatter;
+    private LocalDate ld ;
+    private int bani;
+    private Casa[] casa ;
+    private LifeSimulator ls;
+    int indexcasa;
+    int indexhh;
+    public ThreadMinus()
+    {
+        meseriigui = Meseriigui.getInstance();
+        //casagui= CasaGUI.getInstance();
+        ls= LifeSimulator.getInstance();
+        casa = ls.getCase();
+        indexcasa = citireFisierInt("ProiectMititeanPadureanSimon/src/Ionut/resources/Home","succes","eroare");
+        //indexcasa = casagui.getIndex();
+        mp = MeniuPrincipal.getInstance();
+        hh = Hh.getInstance();
+        datalabelpp = mp.getLabelData();
+        banilabelpp = mp.getLabelBani();
+        //banilabelCasa = casagui.getLabelBani();
+        banilabelMeserii = meseriigui.getLabelBani();
+        banilabelhh = hh.getLabelBani();
+        s = datalabelpp.getText();
+        formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        ld = LocalDate.parse(s, formatter);
+        bani = Integer.parseInt(banilabelpp.getText());
+        //hhlabelCasa = casagui.getLabelHh();
+        hhlabelHh = hh.getLabelHh();
+        hhlabelMp = mp.getLabelHh();
+        hhlabelMeserii = meseriigui.getLabelHh();
+        indexhh = 0;
+    }
+    public void run()
+    {
+
+        while(true)
+        {
+            //indexcasa = casagui.getIndex();
+            formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            bani = Integer.parseInt(banilabelpp.getText());
+            ld = LocalDate.parse(datalabelpp.getText(), formatter);
+            if(indexhh == 14)
+            {
+                setProcentHh();
+                indexhh = 0;
+            }
+            else
+            {
+                indexhh++;
+            }
+            /*if(isPayTime(ld)) {
+                bani -= casa[indexcasa].getChirie();
+                banilabelpp.setText("" + bani);
+                banilabelMeserii.setText("" + bani);
+                banilabelhh.setText("" + bani);
+                banilabelCasa.setText("" + bani);
+                scriereFisier("ProiectMititeanPadureanSimon/src/Ionut/resources/Bani.txt", bani, "succes", "eroare");
+            }*/
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private void setProcentHh()
+    {
+        int hhtemp = Integer.parseInt(hhlabelMp.getText());
+        hhtemp -= 6;
+        hhlabelMp.setText(hhtemp+"");
+        hhlabelHh.setText(hhtemp+"");
+        hhlabelCasa.setText(hhtemp+"");
+        hhlabelMeserii.setText(hhtemp+"");
+        scriereFisier("ProiectMititeanPadureanSimon/src/Ionut/resources/HH.txt",hhtemp,"succes","eroare");
+    }
+    private boolean isPayTime(LocalDate ld)
+    {
+        String sir = citireFisierString("ProiectMititeanPadureanSimon/src/Ionut/resources/HomeData","succes","eroare");
+        LocalDate ldchirie = LocalDate.parse(sir, formatter);
+        int monthnow = ld.getMonthValue();
+        int yearnow = ld.getYear();
+        int daynow = ld.getDayOfMonth();
+        int daychire = ldchirie.getDayOfMonth();
+        int monthchirie = ldchirie.getMonthValue();
+        int yearchirie = ldchirie.getYear();
+        if(yearnow == yearchirie)
+        {
+            if(monthnow > monthchirie)
+            {
+                if(daynow == 20)
+                {
+                    return true;
+                }
+            }
+        }
+        else if( daynow == 20) return true;
+        return false;
+    }
+}
